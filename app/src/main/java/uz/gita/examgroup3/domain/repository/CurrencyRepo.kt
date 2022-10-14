45e5b49cc778248a50cf3c3d4457a02d2451e93f
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2022-10-15 01:26:02 +0500

    -deleted CurrencyRepo.kt

--- app/src/main/java/uz/gita/examgroup3/domain/repository/CurrencyRepo.kt
+++
@@ -1,9 +0,0 @@
-package uz.gita.examgroup3.domain.repository
-
-import kotlinx.coroutines.flow.Flow
-
-interface CurrencyRepo {
-    fun updateCurrency(currency: String)
-
-    fun getCurrency(): Flow<String>
-}
