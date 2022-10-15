7a29bbd9f121ab220a7e2e8eefea58395dc0e814
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2022-10-15 22:03:58 +0500

    -change TransactionUseCase

--- app/src/main/java/uz/gita/examgroup3/domain/usecase/impl/TransactionUseCaseImpl.kt
+++ app/src/main/java/uz/gita/examgroup3/domain/usecase/impl/TransactionUseCaseImpl.kt
@@ -1,12 +1,22 @@
 package uz.gita.examgroup3.domain.usecase.impl
 
+import kotlinx.coroutines.flow.Flow
+import uz.gita.examgroup3.data.local.room.entity.CategoryEntity
 import uz.gita.examgroup3.data.local.room.entity.ExpenditureEntity
+import uz.gita.examgroup3.domain.repository.CategoryRepository
 import uz.gita.examgroup3.domain.repository.ExpenditureRepository
 import uz.gita.examgroup3.domain.usecase.TransactionUseCase
 import javax.inject.Inject
 
-class TransactionUseCaseImpl @Inject constructor(private val expenditureRepository: ExpenditureRepository) :
+class TransactionUseCaseImpl @Inject constructor(
+    private val expenditureRepository: ExpenditureRepository,
+    private val categoryRepository: CategoryRepository
+) :
     TransactionUseCase {
     override fun saveTransaction(expenditureEntity: ExpenditureEntity) =
         expenditureRepository.insertExpenditure(expenditureEntity)
+
+    override fun getExCategories(): Flow<List<CategoryEntity>> = categoryRepository.getExCategories()
+
+    override fun getIncCategories(): Flow<List<CategoryEntity>> = categoryRepository.getIncCategories()
 }
