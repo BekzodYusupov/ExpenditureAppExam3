a2b5d5e748aa7ce2a00729424d58690ebb536028
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2022-10-15 22:11:14 +0500

    -completed EditCategoryUseCase

--- app/src/main/java/uz/gita/examgroup3/domain/usecase/EditCategoryUseCase.kt
+++ app/src/main/java/uz/gita/examgroup3/domain/usecase/EditCategoryUseCase.kt
@@ -1,4 +1,14 @@
 package uz.gita.examgroup3.domain.usecase
 
+import kotlinx.coroutines.flow.Flow
+import uz.gita.examgroup3.data.local.room.entity.CategoryEntity
+
 interface EditCategoryUseCase {
+    fun insertCategory(categoryEntity: CategoryEntity)
+
+    fun deleteCategory(categoryEntity: CategoryEntity)
+
+    fun getExCategories(): Flow<List<CategoryEntity>>
+
+    fun getIncCategories(): Flow<List<CategoryEntity>>
 }
