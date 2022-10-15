913919eb13cdb2ced5389fb71755185356450b49
Hasanboy Abdusattorov <hasanboy.goo@gmail.com>
2022-10-15 22:55:55 +0500

    -qo'shimchalar qo'shildi UseCaseModule

--- app/src/main/java/uz/gita/examgroup3/di/UseCaseModule.kt
+++ app/src/main/java/uz/gita/examgroup3/di/UseCaseModule.kt
@@ -4,10 +4,8 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import uz.gita.examgroup3.domain.usecase.IntroUseCase
-import uz.gita.examgroup3.domain.usecase.SplashUseCase
-import uz.gita.examgroup3.domain.usecase.impl.IntroUseCaseImpl
-import uz.gita.examgroup3.domain.usecase.impl.SplashUseCaseImpl
+import uz.gita.examgroup3.domain.usecase.*
+import uz.gita.examgroup3.domain.usecase.impl.*
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -18,4 +16,16 @@ interface UseCaseModule {
 
     @Binds
     fun bindsSplash(impl: SplashUseCaseImpl): SplashUseCase
+
+    @Binds
+    fun bindsCurrency(impl: CurrencyUseCaseImpl): CurrencyUseCase
+
+    @Binds
+    fun bindsTransaction(impl: TransactionUseCaseImpl): TransactionUseCase
+
+    @Binds
+    fun bindsEditCategory(impl: EditCategoryUseCaseImpl): EditCategoryUseCase
+
+    @Binds
+    fun bindsCategory(impl: CategoryUseCaseImpl): CategoryUseCase
 }
